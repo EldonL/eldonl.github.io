bf0e3835c52339f8054da5a7dd0e5c99033fe26e
EldonL <eldonl@sfu.ca>
2018-10-13 02:31:41 -0700

    changing spideyhero references to avoid IP issues

--- myscript.js
+++ myscript.js
@@ -1,4 +1,16 @@
-function yetAnotherAlert(textToAlert){
-   alert(textToAlert);
+var images =['SpideyHeroSwing.PNG','SpideyHeroGoblinBattle.PNG','SpideyHeroPufferFish.PNG','SpideyHeroWin.PNG'];
+function nextImage() {
+	var img = document.getElementById("slideimage");
+	var imgname = img.name.split("_");
+	var index = imgname[1];
+	if(index ==images.length-1)
+	{
+		index =0; 
+	}
+	else
+	{	
+		index++;
+	}
+	img.src = images[index];
+	img.name = "image_" + index; 
 }
-yetAnotherAlert("This is Chapter2");
