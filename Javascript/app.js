554081fbe63e15f26002d963bfccf1004bf19808
unknown <eldonlin24@gmail.com>
2023-02-17 16:45:57 -0800

    Add logic to filter tabs in personal project section. However, the filter currently does not remove the "show" class when we select another tab. If try filterSelection(ar) for example, in the console, you can see that only ar items gets "show" added

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,23 +1,57 @@
-function myFunction() {
-    document.getElementById("hiddenMessage").style.display = 'block';
-}
-
-function countDown() {
-    var currentVal = document.getElementById("countDownButton").innerHTML;
-    var newVal= 0;
-    if (currentVal>0)
-    {
-        var newVal = currentVal - 1;
+filterSelection('all')
+function filterSelection(filterType){
+ 
+    var elementsName, i;
+    elementsName = document.getElementsByClassName("filterDiv");
+    if(filterType=="all"){
+        filterType = "";
     }
-        document.getElementById("countDownButton").innerHTML = newVal;
+    for(i=0; i < elementsName.length; i++){
+        RemoveClass(elementsName[i], "show");
+        if(elementsName[i].className.indexOf(filterType) > -1){
+            AddClass(elementsName[i], "show");
+        } 
+
+    }
 }
 
-function TurnOnLight() {
-    document.getElementById("myImage").src='BatPowerGif.gif';
+//show filtered elements
+function AddClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i =0; i < arr2.length; i++){
+        if(arr1.indexOf(arr2[i]) == -1) {
+                element.className += " " + arr2[i];
+            
+        }
+    }
 }
 
-function TurnOffLight(){
-    document.getElementById("myImage").src='BookAliveIntro.PNG';
-    
-    
+//hide elements not selected
+function RemoveClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i =0; i < arr2.length; i++){
+       while(arr1.indexOf(arr2[i]) > -1) {
+        console.log(i + " something");
+        arr1.splice(arr1.indexOf(arr2[i], 1));
+       }
+    }
+    element.class = arr1.join(" ");
+}
+
+//Add active class to the current control button to highlight it
+
+ var btnContainer = document.getElementById("personalProjectsTabsContainer");
+ var btns = btnContainer.getElementsByClassName("btnFilterTab");
+for(var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(
+             " active", "");
+             this.className += " active";
+        
+    });
 }
