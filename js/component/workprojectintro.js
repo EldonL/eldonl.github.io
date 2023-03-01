0cd1f6a1123040bcab6b61d167d67065c0e12ec1
unknown <eldonlin24@gmail.com>
2023-03-01 00:42:08 -0800

    create a work project intro and work project star so that so that it is separated from personal project intro and personal project star even though the setup is the same

---
+++ js/component/workprojectintro.js
@@ -0,0 +1,283 @@
+const workProjectIntroTemplate = document.createElement('template');
+
+workProjectIntroTemplate.innerHTML = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <style>
+  
+  .bi{
+    cursor: pointer;
+  }
+
+
+  
+  .title .arrows{
+    display:flex;
+    justify-content:center;
+    column-gap: 3em;
+  }
+
+  .title .arrows .arrow .text{
+    font-size: 1.25em;
+  }
+
+  .title .arrows .arrow .bi{
+    font-size: 6em;
+
+  }
+
+  .title .header{
+    font-size:3.25em;
+    padding-left:1rem;
+    font-weight:bold;
+  }
+
+  .covers{
+    padding-left:1em;
+  }
+
+  .websiteButtons{
+    padding-left:1em;
+  }
+
+  .websiteButtons .websiteButton{
+      padding-top: 1em;
+  }
+
+  .descriptionsection .description{
+    padding-left: 1em;
+    padding-top: 1em;
+  }
+
+  .descriptionsection .description .header{
+    font-size:2.5em;
+    font-weight:bold;
+    padding-bottom: 0.1em;
+  }
+
+  .descriptionsection .description .content{
+    font-size:1.5em;
+  }
+
+  .descriptionsection .subinfos{
+    padding-top: 1em;
+    padding-left: 1em;
+  }
+
+
+  .descriptionsection .subinfos .subtitle{
+    font-size:2em;
+    font-weight:bold;
+    padding-top: 0.5em;
+    padding-bottom: 0.1em;
+  }
+
+  .descriptionsection .subinfos .content{
+    font-size:1.5em;
+  }
+
+  @media screen and (min-width: 1024px){
+    .title{
+      text-align:center;
+    }
+
+    .covers{
+      text-align:center;
+    }
+    
+    .descriptionsection{
+      text-align:center;
+    }
+
+    .descriptionsection .description .content{
+      text-align:left;
+    }
+    .descriptionsection .subinfos .subtitle{
+      font-size:2em;
+    }
+    
+  
+    .descriptionsection .subinfos .content{
+      font-size:2em;
+    }
+
+    .descriptionsection .subinfos{
+      display:grid;
+      justify-content:center;
+    }
+    
+    .descriptionsection .subinfos .subinfo{
+      display:flex;
+      align-items:flex-end;
+      justify-content:left;
+      flex-wrap:wrap;
+
+    }
+  }
+
+  @media screen and (min-width: 1440px){  
+    .title .arrows{
+      float:right;
+      padding-top: 2em;
+      padding-right:1em;
+    }
+
+      .title .header{
+        text-align:center;
+        float:left;
+        font-size: 7em;
+        padding-left:1rem;
+    }
+
+    .covers{
+      clear:both;
+
+    }
+
+    .websiteButtons{
+      display:flex;
+      column-gap:1em;
+    }
+
+    .descriptionsection{
+      display:flex;
+      justify-content:space-between;
+      padding-right:1em;
+    }
+
+    
+    .descriptionsection .description .header{
+      font-size:3.5em;
+
+    }
+
+    .descriptionsection .description .content{
+      font-size:2.5em;
+      width:30em;
+    }
+  
+    .descriptionsection .subinfos .subtitle{
+      font-size:2em;
+      padding-right:0.5em;
+      padding-top: 0em;
+
+    }
+    
+  
+    .descriptionsection .subinfos .content{
+      font-size:2em;
+    }
+
+    .descriptionsection .subinfos .subinfo{
+      display:flex;
+      align-items:flex-end;
+      flex-wrap:wrap;
+
+    }
+  
+  }
+
+
+  </style>
+
+  <div class="container">
+  <div class="title">
+      <div class="arrows">
+          <div class="arrow left-arrow">
+              <div class="text">prev project</div>
+              <i class="bi bi-arrow-left"></i>
+          </div>
+          <div class="arrow right-arrow">
+              <div class="text">next project</div>
+              <i class="bi bi-arrow-right"></i>
+          </div>
+      </div>
+      <div class="header">
+          <div>
+              <slot name="title"></slot>
+          </div>
+      </div>
+
+  </div>
+  <div class="covers">
+      <div class="coverImage">
+          <slot name="webpageImage"></slot>
+      </div>
+      <div class="coverVideo">
+          <slot name="webpageVideo"></slot>
+      </div>
+  </div>
+
+
+  <div class="descriptionsection">
+      <div class="description">
+          <div class="header">Description</div>
+          <div class="content">
+              <slot name="description"></slot>
+          </div>
+      </div>
+      <div class=buttonsubinfo>
+          <div class="websiteButtons">
+              <div class="websiteButton">
+                  <slot name="videoNoWork"></slot>
+              </div>
+              <div class="websiteButton">
+                  <slot name="websiteButton"></slot>
+              </div>
+          </div>
+          <div class="subinfos">
+              <div class="platform subinfo">
+                  <div class="subtitle">Platform:</div>
+                  <div class="content">
+                      <slot name="platform"></slot>
+                  </div>
+              </div>
+              <div class="language subinfo">
+                  <div class="subtitle">Language:</div>
+                  <div class="content">
+                      <slot name="languages"></slot>
+                  </div>
+              </div>
+              <div class="tools subinfo">
+                  <div class="subtitle">Tools:</div>
+                  <div class="content">
+                      <slot name="tools"></slot>
+                  </div>
+              </div>
+              <div class="timeline subinfo">
+                  <div class="subtitle">TimeLine:</div>
+                  <div class="content">
+                      <slot name="timeline"></slot>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+
+
+
+
+
+</div>
+`;
+
+
+
+class WorkProjectIntro extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(workProjectIntroTemplate.content);
+      
+
+    }
+  
+
+  }
+  
+
+
+
+  customElements.define('workprojectintro-component', WorkProjectIntro);
---
+++ js/component/workprojectstar.js
@@ -0,0 +1,116 @@
+const workProjectStarTemplate = document.createElement('template');
+
+workProjectStarTemplate.innerHTML = `
+<style>
+
+  .starblocks .starblock{
+    padding-left:1em;
+    padding-bottom:1em;
+    padding-top:1em;
+  }
+
+  .starblocks .starblock .title{
+    font-weight:bold;
+    font-size: 2.5em;
+    text-align:center;
+  }
+
+  .starblocks .starblock .contentimages .content{
+    font-size: 1.5em;
+    text-align:left;
+  }
+
+  .starblocks .starblock .contentimages .aimage{
+
+    text-align:center;
+  }
+
+  @media screen and (min-width: 1440px){  
+    .starblocks .starblock{
+      padding-left:1em;
+      padding-bottom:1em;
+      padding-top:1em;
+      display:grid;
+      grid-template-columns: 15% 85%;
+    }
+
+    .starblocks .starblock .title{
+
+      text-align:left;
+    }
+
+    .starblocks .starblock .contentimages{
+      display:grid;  
+    }
+    .starblocks .starblock .contentimages .aimage{
+
+      text-align:left;
+    }
+    
+  }
+
+
+
+</style>
+<div class="starblocks">
+    <div class="starblock">
+        <div class="title">Situation</div>
+        <div class="contentimages">
+            <div class="content"><slot name="situation"></slot></div>
+        </div>
+
+    </div>
+    <div class="starblock">
+        <div class="title">Task</div>
+        <div class="contentimages">
+            <div class="content"><slot name="task"></slot></div>
+        </div>
+    </div>
+    <div class="starblock">
+        <div class="title">Action</div>
+        <div class="contentimages">
+            <div class="content"><slot name="action1text"></slot></div>
+            <div class="aimage"><slot name="action1image"></slot></div>
+            <div class="content"><slot name="action2text"></slot></div>
+            <div class="aimage"><slot name="action2image"></slot></div>
+            <div class="content"><slot name="action3text"></slot></div>
+            <div class="aimage"><slot name="action3image"></slot></div>
+        </div>
+
+    </div>
+    <div class="starblock">
+        <div class="title">Result</div>
+        <div class="contentimages">
+            <div class="content"><slot name="result1text"></slot></div>
+            <div class="aimage"><slot name="result1image"></slot></div>
+            <div class="content"><slot name="result2text"></slot></div>
+            <div class="aimage"><slot name="result2image"></slot></div>
+            <div class="content"><slot name="result3text"></slot></div>
+            <div class="aimage"><slot name="result3image"></slot></div>
+        </div>
+
+    </div>
+
+</div>
+
+`;
+
+
+
+class WorkProjectStar extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(workProjectStarTemplate.content);
+      
+
+    }
+  
+
+  }
+  
+
+
+
+  customElements.define('workprojectstar-component', WorkProjectStar);
