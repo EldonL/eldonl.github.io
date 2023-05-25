7d0742d4b1ab562dbbaa686c3a31101fc8c2058a
unknown <eldonlin24@gmail.com>
2023-05-24 19:10:58 -0700

    Adding blog css, js and html to prep for blog posts

---
+++ js/component/blogcontent.js
@@ -0,0 +1,121 @@
+const blogContentTemplate = document.createElement('template');
+
+blogContentTemplate.innerHTML = `
+<style>
+
+  .diarypostblocks .diarypostblock{
+    padding-left:1em;
+    padding-bottom:1em;
+    padding-top:1em;
+  }
+
+
+
+  .diarypostblocks .diarypostblock .contentimages .content{
+    font-size: 1.5em;
+    text-align:left;
+  }
+
+  .diarypostblocks .diarypostblock .contentimages .aimage{
+
+    text-align:center;
+  }
+
+  @media screen and (min-width: 1440px){  
+    .diarypostblocks .diarypostblock{
+      padding-left:1em;
+      padding-bottom:1em;
+      padding-top:1em;
+      display:grid;
+      grid-template-columns: 15% 85%;
+    }
+
+
+    .diarypostblocks .diarypostblock .contentimages{
+      display:grid;  
+    }
+    .diarypostblocks .diarypostblock .contentimages .aimage{
+
+      text-align:left;
+    }
+    
+  }
+
+
+
+</style>
+<div class="diarypostblocks">
+    <div class="diarypostblock">
+
+        <div class="contentimages">
+            <div class="content"><slot name="action1text"></slot></div>
+            <div class="aimage"><slot name="action1image"></slot></div>
+            <div class="content"><slot name="action2text"></slot></div>
+            <div class="aimage"><slot name="action2image"></slot></div>
+            <div class="content"><slot name="action3text"></slot></div>
+            <div class="aimage"><slot name="action3image"></slot></div>
+            <div class="content"><slot name="action4text"></slot></div>
+            <div class="aimage"><slot name="action4image"></slot></div>
+            <div class="content"><slot name="action5text"></slot></div>
+            <div class="aimage"><slot name="action5image"></slot></div>
+            <div class="content"><slot name="action6text"></slot></div>
+            <div class="aimage"><slot name="action6image"></slot></div>
+            <div class="content"><slot name="action7text"></slot></div>
+            <div class="aimage"><slot name="action7image"></slot></div>
+            <div class="content"><slot name="action8text"></slot></div>
+            <div class="aimage"><slot name="action8image"></slot></div>
+            <div class="content"><slot name="action9text"></slot></div>
+            <div class="aimage"><slot name="action9image"></slot></div>
+            <div class="content"><slot name="action10text"></slot></div>
+            <div class="aimage"><slot name="action10image"></slot></div>
+            <div class="content"><slot name="action11text"></slot></div>
+            <div class="aimage"><slot name="action11image"></slot></div>
+            <div class="content"><slot name="action12text"></slot></div>
+            <div class="aimage"><slot name="action12image"></slot></div>
+            <div class="content"><slot name="action13text"></slot></div>
+            <div class="aimage"><slot name="action13image"></slot></div>
+            <div class="content"><slot name="action14text"></slot></div>
+            <div class="aimage"><slot name="action14image"></slot></div>
+        </div>
+
+    </div>
+    <div class="diarypostblock">
+
+        <div class="contentimages">
+            <div class="content"><slot name="result1text"></slot></div>
+            <div class="aimage"><slot name="result1image"></slot></div>
+            <div class="content"><slot name="result2text"></slot></div>
+            <div class="aimage"><slot name="result2image"></slot></div>
+            <div class="content"><slot name="result3text"></slot></div>
+            <div class="aimage"><slot name="result3image"></slot></div>
+            <div class="content"><slot name="result4text"></slot></div>
+            <div class="aimage"><slot name="result4image"></slot></div>
+            <div class="content"><slot name="result5text"></slot></div>
+            <div class="aimage"><slot name="result5image"></slot></div>
+        </div>
+
+    </div>
+
+</div>
+
+`;
+
+
+
+class BlogContent extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(blogContentTemplate.content);
+      
+
+    }
+  
+
+  }
+  
+
+
+
+  customElements.define('blogcontent-component', BlogContent);
---
+++ js/component/blogintro.js
@@ -0,0 +1,254 @@
+const blogIntroTemplate = document.createElement('template');
+
+blogIntroTemplate.innerHTML = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <style>
+  
+  .bi{
+    cursor: pointer;
+  }
+
+   .directory{
+    text-align: left;
+    padding-left: 1em;
+    font-size: 1.25em;
+   }
+  
+  .title .arrows{
+    display:flex;
+    justify-content:center;
+    column-gap: 3em;
+    display:none; //display none because we did not figure out a way to not do manual next
+  }
+
+  .title .arrows .arrow .text{
+    font-size: 1.25em;
+  }
+
+  .title .arrows .arrow .bi{
+    font-size: 6em;
+
+  }
+
+  .title .header{
+    font-size:3.25em;
+    padding-left:1rem;
+    font-weight:bold;
+  }
+
+  .covers{
+    padding-left:1em;
+  }
+
+  .websiteButtons{
+    padding-left:1em;
+  }
+
+  .websiteButtons .websiteButton{
+      padding-top: 1em;
+  }
+
+  .descriptionsection .description{
+    padding-left: 1em;
+    padding-top: 1em;
+  }
+
+  .descriptionsection .description .header{
+    font-size:2.5em;
+    font-weight:bold;
+    padding-bottom: 0.1em;
+  }
+
+  .descriptionsection .description .content{
+    font-size:1.5em;
+  }
+
+  .descriptionsection .subinfos{
+    padding-top: 1em;
+    padding-left: 1em;
+  }
+
+
+  .descriptionsection .subinfos .subtitle{
+    font-size:2em;
+    font-weight:bold;
+    padding-top: 0.5em;
+    padding-bottom: 0.1em;
+  }
+
+  .descriptionsection .subinfos .content{
+    font-size:1.5em;
+  }
+
+  @media screen and (min-width: 1024px){
+    .title{
+      text-align:left;
+    }
+
+    .covers{
+      text-align:center;
+    }
+    
+    .descriptionsection{
+      text-align:center;
+    }
+
+    .descriptionsection .description .content{
+      text-align:left;
+    }
+    .descriptionsection .subinfos .subtitle{
+      font-size:2em;
+    }
+    
+  
+    .descriptionsection .subinfos .content{
+      font-size:2em;
+    }
+
+    .descriptionsection .subinfos{
+      display:grid;
+      justify-content:center;
+    }
+    
+    .descriptionsection .subinfos .subinfo{
+      display:flex;
+      align-items:flex-end;
+      justify-content:left;
+      flex-wrap:wrap;
+
+    }
+  }
+
+  @media screen and (min-width: 1440px){  
+    .title .arrows{
+      float:right;
+      padding-top: 2em;
+      padding-right:1em;
+    }
+
+      .title .header{
+        text-align:left;
+        float:left;
+        font-size: 7em;
+        padding-left:1rem;
+    }
+
+    .covers{
+      clear:both;
+
+    }
+
+    .websiteButtons{
+      display:flex;
+      column-gap:1em;
+    }
+
+    .descriptionsection{
+      display:flex;
+      justify-content:space-between;
+      padding-right:1em;
+    }
+
+    
+    .descriptionsection .description .header{
+      font-size:3.5em;
+      text-align:left;
+    }
+
+    .descriptionsection .description .content{
+      font-size:1.5em;
+      width:45em;
+    }
+  
+    .descriptionsection .subinfos .subtitle{
+      font-size:1.5em;
+      padding-right:0.5em;
+      padding-top: 0em;
+
+    }
+    
+  
+    .descriptionsection .subinfos .content{
+      font-size:1.5em;
+    }
+
+    .descriptionsection .subinfos .subinfo{
+      display:flex;
+      align-items:flex-end;
+      flex-wrap:wrap;
+
+    }
+  
+  }
+
+
+  </style>
+
+  <div class="container">
+  <p class="directory">professional experience projects</p>
+  <div class="title">
+      <div class="arrows">
+          <div class="arrow left-arrow">
+              <div class="text">prev project</div>
+              <i class="bi bi-arrow-left"></i>
+          </div>
+          <div class="arrow right-arrow">
+              <div class="text">next project</div>
+              <i class="bi bi-arrow-right"></i>
+          </div>
+      </div>
+      <div class="header">
+          <div>
+              <slot name="title"></slot>
+          </div>
+      </div>
+
+  </div>
+  <div class="covers">
+      <div class="coverImage">
+          <slot name="webpageImage"></slot>
+      </div>
+      <div class="coverVideo">
+          <slot name="webpageVideo"></slot>
+      </div>
+  </div>
+
+
+  <div class="descriptionsection">
+      <div class="description">
+          <div class="header">Description</div>
+          <div class="content">
+              <slot name="description"></slot>
+          </div>
+      </div>
+      
+  </div>
+
+
+
+
+
+
+
+</div>
+`;
+
+
+
+class BlogIntro extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(blogIntroTemplate.content);
+      
+
+    }
+  
+
+  }
+  
+
+
+
+  customElements.define('blogintro-component', BlogIntro);
