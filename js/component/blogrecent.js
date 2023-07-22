3e0a968f822dbfb6cc69975a395ef5fce5838b23
unknown <eldonlin24@gmail.com>
2023-07-21 22:46:58 -0700

    Add blog 8 - collecting coints

--- js/component/blogrecent.js
+++ js/component/blogrecent.js
@@ -16,6 +16,16 @@ blogRecentTemplate.innerHTML = `
         <div class="recentblog">
         <div class="filterblog">
             <div class="blogcontent">
+                <a href="blogfirstandroidgame8.html"><img class="image" src="images_videos/blog/FirstAndroidGame/threetypesofcoins.PNG"
+                    alt="cover image"></a>
+                <div class="imagetext">Click to see more</div>
+                <div class="bottom">
+                    <p class="title">First Android Game - Part 8 - Collecting Coins</p>
+                </div>
+            </div>
+        </div>
+        <div class="filterblog">
+            <div class="blogcontent">
                 <a href="blogfirstandroidgame7.html"><img class="image" src="images_videos/blog/FirstAndroidGame/enemycannon.PNG"
                     alt="cover image"></a>
                 <div class="imagetext">Click to see more</div>
