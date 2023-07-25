ca182424df9640c67423093052a0069e1a38e6bb
unknown <eldonlin24@gmail.com>
2023-07-25 14:57:10 -0700

    adding blog 9 - fixing enemy collisions

--- js/component/blogrecent.js
+++ js/component/blogrecent.js
@@ -16,6 +16,16 @@ blogRecentTemplate.innerHTML = `
         <div class="recentblog">
         <div class="filterblog">
             <div class="blogcontent">
+                <a href="blogfirstandroidgame9.html"><img class="image" src="images_videos/blog/FirstAndroidGame/photon196.gif"
+                    alt="cover image"></a>
+                <div class="imagetext">Click to see more</div>
+                <div class="bottom">
+                    <p class="title">First Android Game - Part 9 - Fixing Enemy Collisions</p>
+                </div>
+            </div>
+        </div>
+        <div class="filterblog">
+            <div class="blogcontent">
                 <a href="blogfirstandroidgame8.html"><img class="image" src="images_videos/blog/FirstAndroidGame/threetypesofcoins.PNG"
                     alt="cover image"></a>
                 <div class="imagetext">Click to see more</div>
