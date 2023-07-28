ef0466e7c7bbbacd535b2a4ce68dac0bf32e8f21
unknown <eldonlin24@gmail.com>
2023-07-27 19:13:24 -0700

    instead of individually scroll in filterdiv, scroll in all because there is a bug where if you have not scrolled all the way and switch tabs then three will be projects missing

--- js/index.js
+++ js/index.js
@@ -6,6 +6,6 @@ ScrollReveal({
 })
 
 ScrollReveal().reveal('.hero-section .left-col, .header,.footer,.filterTabs', {origin:'top'});
-ScrollReveal().reveal('.hero-section .right-col, .all, .blog, .filterDiv, .volunteer, .skills-info,.jobs', {origin:'bottom'});
+ScrollReveal().reveal('.hero-section .right-col, .all, .blog, .personalProjectFilteredContainer, .volunteer, .skills-info,.jobs', {origin:'bottom'});
 ScrollReveal().reveal('.about-elements .about-me-img', {origin:'left'});
 ScrollReveal().reveal('.about-elements .content', {origin:'right'});
