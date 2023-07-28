1761986f7f73dcf5840a6f5064c9fbea75f544bf
unknown <eldonlin24@gmail.com>
2023-07-27 21:35:37 -0700

    add in typed js to show different positions using a typed format in the hero section

--- js/index.js
+++ js/index.js
@@ -9,3 +9,12 @@ ScrollReveal().reveal('.hero-section .left-col, .header,.footer,.filterTabs', {o
 ScrollReveal().reveal('.hero-section .right-col, .all, .blog, .personalProjectFilteredContainer, .volunteer, .skills-info,.jobs', {origin:'bottom'});
 ScrollReveal().reveal('.about-elements .about-me-img', {origin:'left'});
 ScrollReveal().reveal('.about-elements .content', {origin:'right'});
+
+const multiplepositions= new Typed('.hero-subheadermultiplepositions',{
+    strings:['Software Programmer', "XR Developer", "Systems Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+
