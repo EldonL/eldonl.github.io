8480ffd413e7b8bff6ad47badcee5a7adb0f10a1
unknown <eldonlin24@gmail.com>
2023-07-27 18:50:22 -0700

    make individual work, volunteer and skills have scroll instead of having all of them scroll in

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ ScrollReveal({
     delay: 200
 })
 
-ScrollReveal().reveal('.hero-section .left-col, .header,.footer', {origin:'top'});
-ScrollReveal().reveal('.hero-section .right-col, .all, .blog', {origin:'bottom'});
+ScrollReveal().reveal('.hero-section .left-col, .header,.footer,.filterTabs', {origin:'top'});
+ScrollReveal().reveal('.hero-section .right-col, .all, .blog, .filterDiv, .volunteer, .skills-info,.jobs', {origin:'bottom'});
 ScrollReveal().reveal('.about-elements .about-me-img', {origin:'left'});
 ScrollReveal().reveal('.about-elements .content', {origin:'right'});
