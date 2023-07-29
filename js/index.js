bff0d03c86a1cb5f5f9dabcd73babab923412a9c
unknown <eldonlin24@gmail.com>
2023-07-29 04:48:17 -0700

    remove scroll reveal for about me image and volunteer section because they keep not appearing for android and pc respectively

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ ScrollReveal({
 })
 
 ScrollReveal().reveal('.about-elements .content,.hero-section .left-col, .header,.filterTabs', {origin:'top'});
-ScrollReveal().reveal('.about-elements .about-me-img,.hero-section .right-col, .all, .blog, .personalProjectFilteredContainer, .volunteer, .skills-info,.jobs', {origin:'bottom'});
+ScrollReveal().reveal('.hero-section .right-col, .all, .blog, .personalProjectFilteredContainer, .skills-info,.jobs', {origin:'bottom'});
 
 
 const multiplepositions= new Typed('.hero-typejs',{
