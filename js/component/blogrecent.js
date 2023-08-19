6fc12ff0cfe8e938c5f285b79ce481f282343f3f
unknown <eldonlin24@gmail.com>
2023-08-19 05:05:13 -0700

    add blog 15 and explain how I continue spawning coins on client even when master closes the app (did not handle selecting quit) by adding OnMasterClientSwitch callback. Note exisiting coins that spawned from OG master will freeze

--- js/component/blogrecent.js
+++ js/component/blogrecent.js
@@ -16,6 +16,16 @@ blogRecentTemplate.innerHTML = `
         <div class="recentblog">
         <div class="filterblog">
             <div class="blogcontent">
+                <a href="blogfirstandroidgame15.html"><img class="image" src="images_videos/blog/FirstAndroidGame/mastertransfer2.gif"
+                alt="cover image"></a>
+                <div class="imagetext">Click to see more</div>
+                <div class="bottom">
+                    <p class="title">First Android Game - Part 15 - Changing Masters</p>
+                </div>
+            </div>
+        </div>        
+        <div class="filterblog">
+            <div class="blogcontent">
                 <a href="blogfirstandroidgame14.html"><img class="image" src="images_videos/blog/FirstAndroidGame/syncbg6.gif"
                 alt="cover image"></a>
                 <div class="imagetext">Click to see more</div>
