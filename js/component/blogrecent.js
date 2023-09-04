5cacc21a74c10ad511450d9addbfdc391b00cc35
unknown <eldonlin24@gmail.com>
2023-09-03 19:47:40 -0700

    add blog 19 - adding is player ready in lobby. Discussed what I did and the bugs I encountered- no solutions yet

--- js/component/blogrecent.js
+++ js/component/blogrecent.js
@@ -16,6 +16,16 @@ blogRecentTemplate.innerHTML = `
         <div class="recentblog">
         <div class="filterblog">
             <div class="blogcontent">
+                <a href="blogfirstandroidgame19.html"><img class="image" src="images_videos/blog/FirstAndroidGame/readytoplay2.PNG"
+                alt="cover image"></a>
+                <div class="imagetext">Click to see more</div>
+                <div class="bottom">
+                    <p class="title">First Android Game - Part 19 - Adding ready to play</p>
+                </div>
+            </div>
+        </div>        
+        <div class="filterblog">
+            <div class="blogcontent">
                 <a href="blogfirstandroidgame18.html"><img class="image" src="images_videos/blog/FirstAndroidGame/syncgameovermenu11.PNG"
                 alt="cover image"></a>
                 <div class="imagetext">Click to see more</div>
