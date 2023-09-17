c2e790dea447d69ee4d35ea88bf997d1d2b84751
unknown <eldonlin24@gmail.com>
2023-09-16 17:04:39 -0700

    explain what I did for plane detection by following the AR samples foundation

--- js/component/blogrecent.js
+++ js/component/blogrecent.js
@@ -16,6 +16,16 @@ blogRecentTemplate.innerHTML = `
         <div class="recentblog">
         <div class="filterblog">
             <div class="blogcontent">
+                <a href="blogarfoundationsimplearplane.html"><img class="image" src="images_videos/blog/ARFoundationSamples/arcore.jpg"
+                alt="cover image"></a>
+                <div class="imagetext">Click to see more</div>
+                <div class="bottom">
+                    <p class="title">ARCore Plane detection</p>
+                </div>
+            </div>
+        </div>        
+        <div class="filterblog">
+            <div class="blogcontent">
                 <a href="blogfirstandroidgame19.html"><img class="image" src="images_videos/blog/FirstAndroidGame/readytoplay2.PNG"
                 alt="cover image"></a>
                 <div class="imagetext">Click to see more</div>
