28f764c82a339c2e7f18bb127732b7afa91a0597
unknown <eldonlin24@gmail.com>
2026-04-06 19:33:57 -0700

    make bi links go somewhere - changing to href instead of using onclick from app.js

--- js/component/footer2.js
+++ js/component/footer2.js
@@ -6,11 +6,13 @@ footer2Template.innerHTML =`
 <style>
   .bi{
     cursor:pointer;
+
   }
 
   .bi:hover{
     opacity:0.5;
   }
+    
 
   .call-to-action {
    font-size: 1.25rem; 
@@ -23,29 +25,33 @@ footer2Template.innerHTML =`
     display: flex;
     flex-direction: column;
     gap: 5rem;
-
   }
 
   .footer2-section .container .contact-links{
     display:flex; 
     justify-content:center; 
     gap:5rem;
-    font-size: 4rem;
+    font-size: 3rem;
+    text-decoration:none;
   }
 
-
-
+.footer2-section .container .contact-links a {
+  color: inherit;      
+}
 
   </style>
+
   <section class="footer2-section" id="contact">
         <div class="container">
             <div class="ready-to-talk">
                 <h1>Ready to create immersive experiences together?</h1>
                 <a href="https://www.linkedin.com/in/eldon-lin-38a181b0"><button class="call-to-action btn">Contact (LinkedIn)</button></a>
             </div>
+
+
             <div class="contact-links">                
-                <a onclick="OnLinkClick('https://www.youtube.com/channel/UC-igukaTIJFHYS8TNkcJ4lA')"><i class="bi bi-youtube"></i></a>
-                <a onclick="OnLinkClick('https://github.com/EldonL')"><i class="bi bi-github"></i></a>
+                <a href="https://www.youtube.com/channel/UC-igukaTIJFHYS8TNkcJ4lA"><i class="bi bi-youtube"></i></a>
+                <a href="https://github.com/EldonL"><i class="bi bi-github"></i></a>
             </div>
 
 
