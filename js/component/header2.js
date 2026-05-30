bb7d67c9d893ce17478e7142a13af8ed6f4fed36
unknown <eldonlin24@gmail.com>
2026-05-29 23:50:59 -0700

    added a responsive header that turns into hamburger menu if width screen size is small. The burger will transition to x when pressed opening a side menu

--- js/component/header2.js
+++ js/component/header2.js
@@ -5,60 +5,143 @@ header2template.innerHTML=`
 <style>
 
 
-.header {
+.nav_header {
   overflow: hidden;
   background-color: #f1faff;
 
 
 }
 
-.header a {
+.nav_header a {
 
   color: black;
   text-align: center;
-  padding: 1rem;
+  padding: 0.5rem;
 
   text-decoration: none;
   font-size: 1rem; 
-  line-height: 1rem;
-
-
 
 }
 
-.header a.logo {
+.nav_header a.nav_logo {
   font-size: 2rem;
   font-weight: bold;
   float: left;
 }
 
-.header a:hover {
+.nav_header a:hover {
   background-color: #ddd;
-
 }
 
-.header-right a.active {
+.nav_menu {
+  float: right;
+  padding-top: 0.5rem;
+}
+
+.nav_menu ul{
+  display:flex;
+  padding-right:1rem;
+}
+
+.nav_menu ul li{
+  list-style: none;
+}
+
+.nav_menu ul a.active {
   background-color: #d4d4d4;
 }
 
-.header-right {
-  float: right;
-    padding-top: 1rem;
+.nav_hamburger{
+  width:2.625rem;
+  height:1.8382rem;
+  display:none; 
+  flex-direction:column;
+  justify-content: space-around;
+  position:absolute;
+  cursor:pointer;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+
 }
 
+.nav_hamburger_line{
+  height:0.188rem;
+  width: 100%; 
+  background-color: #000000; 
+  border-radius: 0.625rem;
+  transition: all ease-in-out 0.2s;
+}
 
+@media screen and (max-width: 960px){
+    .nav_hamburger{
+      display:flex; 
+      
+    }
+
+    .nav_menu{
+      position:absolute; 
+      display:flex;
+      align-items: center; 
+      background: #f3f4ff;
+      right: -100rem;
+      width:50%; 
+      height: 100vh; //rely on the viewport full screen
+      transition: all ease-in-out 0.4s;
+      z-index:-1;
+      padding-top: 0rem;
+    }
+
+    .nav{
+      flex-direction:column;
+      align-items:flex-start;
+    }
+
+    .nav_menu ul{
+      flex-direction:column;
+      width:100%; 
+    }
+
+    .nav_menu ul li{
+      text-align: center; 
+      padding-bottom: 2rem;
+    }
+
+    .nav_menu.active{
+      right:0;
+    }
+
+    .nav_hamburger.active :nth-child(1){
+      transform:rotate(45deg) translate(0.45rem, 0.2625rem);
+    }
+
+    .nav_hamburger.active :nth-child(2){
+      opacity:0;
+    }
+
+    .nav_hamburger.active :nth-child(3){
+      transform:rotate(-45deg) translate(0.45rem, -0.2625rem);
+    }
+}
 
 </style>
 
-    <div class="header">
-      <a href="index.html" class="logo">ELDON LIN</a>
-      <div class="header-right">
-        <a href="index.html#featuredproject">Featured Projects</a>
-        <a href="index.html#techstacksection">Tech Stack</a>
-        <a href="index.html#workexperience">Work Experiences</a>
-        <a href="index.html#contact">Contact</a>
+    <nav class="nav_header">
+      <a href="index.html" class="nav_logo">ELDON LIN</a>
+      <button class="nav_hamburger" type="button">
+        <span class="nav_hamburger_line"></span>
+        <span class="nav_hamburger_line"></span>
+        <span class="nav_hamburger_line"></span>
+      </button>
+      <div class="nav_menu">
+        <ul>
+          <li class="nav_menu_link"><a href="index.html#featuredproject">Featured Projects</a></li>
+          <li class="nav_menu_link"><a href="index.html#techstacksection">Tech Stack</a></li>
+          <li class="nav_menu_link"><a href="index.html#workexperience">Work Experiences</a></li>
+          <li class="nav_menu_link"><a href="index.html#contact">Contact</a></li>
+        </ul>
       </div>
-    </div>
+    </nav>
 
 `
 
@@ -79,9 +162,40 @@ customElements.define("header2-component",Header2);
 
 const hero = document.querySelector(".hero");//get the headset scroll progress so progress in hdaer2 matches headset
 const header = document.querySelector(".header2");
-
+const headerComponent = document.querySelector("header2-component");
+const shadow = headerComponent.shadowRoot;
+const hamburger = shadow.querySelector(".nav_hamburger");
+const linksContainer = shadow.querySelector(".nav_menu");
+const links = shadow.querySelectorAll(".nav_menu_link");
 
 const sectionIds = ['featuredproject', 'techstacksection', 'workexperience', 'contact'];
+
+
+hamburger.addEventListener("click",()=>{
+   linksContainer.classList.toggle("active");
+   hamburger.classList.toggle("active");
+});
+
+window.addEventListener("resize",()=>{
+  if(window.matchMedia("(max-width:960px)").matches){
+    closeMenu();
+  }
+});
+
+
+if(window.matchMedia("(max-width:960px)").matches){
+    closeMenu();
+  }
+
+function closeMenu(){
+    links.forEach((link)=>{
+    link.addEventListener("click",()=>{
+      linksContainer.classList.remove("active");
+      hamburger.classList.remove("active");
+    });
+  });
+}
+
 
 function getNavLinks() {
   const headerComponent = document.querySelector('header2-component');
@@ -89,7 +203,7 @@ function getNavLinks() {
   
   const links = {};
   sectionIds.forEach(id => {
-    links[id] = headerComponent.shadowRoot.querySelector(`.header-right a[href="index.html#${id}"]`);
+    links[id] = headerComponent.shadowRoot.querySelector(`.nav_menu a[href="index.html#${id}"]`);
   });
   return links;
 }
